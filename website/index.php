5c68f44f64b9e4e5c8497ec01356631389a38214
crynobone <crynobone@gmail.com>
2011-08-17 09:37:27 +0800

    Initial commit - website

---
+++ website/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <?php $mtime = mktime(0, 0, 0, 8, 16, 2011); ?>
+    <title>Mior Muhammad Zaki</title>
+    <link type="text/css" rel="stylesheet" href="assets/css/style.css?<?php echo $mtime; ?>" /> 
+</head>
+<body>
+    <section id="left">
+        <header>
+            <h1>Mior Muhammad Zaki</h1>
+            <p>Mior Muhammad Zaki or better known in the net as crynobone is a PHP, MySQL &amp; JavaScript Developer and the founder of Savvy.UI JavaScript Library and FeedMalaya. In real life, I join the development in a Web 2.0 company in Malaysia developing cool software for aviation industry.</p>
+        </header>
+        <ul>
+            <li>
+                <a href="http://github.com">Github</a>
+                <span>Coding just turn social, check out my commits and projects</span>
+            </li>
+            <li>
+                <a href="http://www.linkedin.com/in/crynobone">Linkedin</a>
+                <span>Join into my professional network</span>
+            </li>
+            <li>
+                <a href="http://crynobone.com/cv.pdf">Resume´</a>
+                <span>Feel free to read my resume´ if you're searching for a programmer.</span>
+            </li>
+            <li>
+                <a href="http://twitter.com/crynobone">Twitter</a>
+                <span>Life in just 140 characters by <a href="http://twitter.com/crynobone">@crynobone</a></span>
+            </li>
+            <li>
+                <a href="http://fml.crynobone.com">Blog</a>
+                <span>A place where I can rant about anything</span>
+            </li>
+        </ul>
+    </section>
+    <link type="text/css" rel="stylesheet" href="//fonts.googleapis.com/css?family=Droid+Sans:regular,bold">
+</body>
+</html>
